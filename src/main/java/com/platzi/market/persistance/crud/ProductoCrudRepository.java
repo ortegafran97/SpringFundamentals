afa86ed3349299d07669fadc60516d50cbf6228e
Franco Ortega <ortegafran97@gmail.com>
2022-08-22 17:20:42 -0300

    Methods added

--- src/main/java/com/platzi/market/persistance/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistance/crud/ProductoCrudRepository.java
@@ -3,6 +3,10 @@ package com.platzi.market.persistance.crud;
 import com.platzi.market.persistance.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
