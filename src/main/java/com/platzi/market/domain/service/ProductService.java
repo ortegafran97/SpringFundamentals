9b085c4cf9c4099787e8ff351e6d154897b5b188
Franco Ortega <ortegafran97@gmail.com>
2022-08-22 17:20:10 -0300

    Constructor added

--- src/main/java/com/platzi/market/domain/service/ProductService.java
+++ src/main/java/com/platzi/market/domain/service/ProductService.java
@@ -11,7 +11,11 @@ import java.util.Optional;
 @Service
 public class ProductService {
     @Autowired
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
 
     public List<Product> getAll(){
