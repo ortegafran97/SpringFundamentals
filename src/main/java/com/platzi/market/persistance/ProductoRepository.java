9b411c08dc859e45f2848c57d7f2ddd11304d677
Franco Ortega <ortegafran97@gmail.com>
2022-08-22 15:15:51 -0300

    Product repository

--- src/main/java/com/platzi/market/persistance/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistance/ProductoRepository.java
@@ -1,2 +1,30 @@
-package com.platzi.market.persistance;public class ProductoRepository {
+package com.platzi.market.persistance;
+
+import com.platzi.market.persistance.crud.ProductoCrudRepository;
+import com.platzi.market.persistance.entity.Producto;
+import org.hibernate.procedure.ProcedureOutputs;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository {
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll(){
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto p){
+        return productoCrudRepository.save(p);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
 }
--- src/main/java/com/platzi/market/persistance/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistance/crud/ProductoCrudRepository.java
@@ -1,2 +1,8 @@
-package com.platzi.market.persistance.crud;public interface ProductoCrudRepository {
+package com.platzi.market.persistance.crud;
+
+import com.platzi.market.persistance.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
 }
