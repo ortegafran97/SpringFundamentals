e13629aa5fed45011a382ab6793d87efddff2643
Franco Ortega <ortegafran97@gmail.com>
2022-08-24 18:28:50 -0300

    Fix

--- src/main/java/com/platzi/market/persistance/entity/ComprasProducto.java
+++ src/main/java/com/platzi/market/persistance/entity/ComprasProducto.java
@@ -16,12 +16,12 @@ public class ComprasProducto {
     private Boolean estado;
 
     @ManyToOne
+    @MapsId("idCompra")
     @JoinColumn(name="id_compra",insertable = false,updatable = false)
     private Compra compra;
 
     @ManyToOne
-    @MapsId("idCompra")
-    @JoinColumn(name="id_compra",insertable = false,updatable = false)
+    @JoinColumn(name="id_producto",insertable = false,updatable = false)
     private Producto producto;
 
 
