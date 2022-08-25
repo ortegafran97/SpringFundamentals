dc19c73eff40da11deb185d762bc6ee5ee558d53
Franco Ortega <ortegafran97@gmail.com>
2022-08-25 13:52:01 -0300

    JWT Configs

--- src/main/java/com/platzi/market/web/security/JWTUtil.java
+++ src/main/java/com/platzi/market/web/security/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.platzi.market.web.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,6 +20,24 @@ public class JWTUtil {
                 .compact();
     }
 
+    public boolean validateToken(String token, UserDetails userDetails){
+        return userDetails.getUsername()
+                .equals(extractUsername(token))
+                 && !isTokenExpired(token);
+    }
 
+    public String extractUsername(String token){
+        return getClaims(token).getSubject();
+    }
 
+    public boolean isTokenExpired(String token){
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String token){
+        return Jwts.parser()
+                .setSigningKey(KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
--- src/main/java/com/platzi/market/web/security/SecurityConfig.java
+++ src/main/java/com/platzi/market/web/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.platzi.market.web.security;
 
 import com.platzi.market.domain.service.PlatziUserDetailsService;
+import com.platzi.market.web.security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,12 +10,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private PlatziUserDetailsService platziUserDetailsService;
+
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -28,7 +34,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .disable()
                 .authorizeHttpRequests()
                 .antMatchers("/**/authenticate").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
