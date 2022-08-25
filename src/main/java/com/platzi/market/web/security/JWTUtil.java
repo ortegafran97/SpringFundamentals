043154ea9f5abeaef8280694a2f6334638725f70
Franco Ortega <ortegafran97@gmail.com>
2022-08-25 10:57:27 -0300

    Component decorator added

--- src/main/java/com/platzi/market/web/security/JWTUtil.java
+++ src/main/java/com/platzi/market/web/security/JWTUtil.java
@@ -3,14 +3,13 @@ package com.platzi.market.web.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+@Component
 public class JWTUtil {
-
     private static final String KEY="pl4tz1";
-
-
     public String generateToken(UserDetails userDetails){
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
